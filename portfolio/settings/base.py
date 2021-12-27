10714e644da7f277fc6649cb6adcd5e8afe335c8
thelimeskies <sammyboy.as@gmail.com>
2021-12-27 20:30:15 +0100

    Added Cloudinary

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -129,9 +129,9 @@ USE_TZ = True
 
 # Cloudinary stuff
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': env('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': env('CLOUDINARY_API_KEY'),
-    'API_SECRET': env('CLOUDINARY_API_SECRET'),
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
